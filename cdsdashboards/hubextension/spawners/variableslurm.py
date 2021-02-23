67a2e7d141c74b46a7a2cb5e466bca089d816d72
Adam Lewis <balast@users.noreply.github.com>
2021-02-23 12:23:32 -0600

    add variableslurm

--- cdsdashboards/hubextension/spawners/variableslurm.py
+++ cdsdashboards/hubextension/spawners/variableslurm.py
@@ -4,11 +4,11 @@ from .variablemixin import VariableMixin, MetaVariableMixin, Command
 
 class VariableSlurmSpawner(SlurmSpawner, VariableMixin, metaclass=MetaVariableMixin):
     default_presentation_cmd = Command(
-        ['jhsingle_native_proxy'],
+        ['jhsingle-native-proxy'],
         allow_none=False,
         help="""
-        The command to run presentations through jhsingle_native_proxy, can be a string or list.
-        Default is ['jhsingle_native_proxy'].
+        The command to run presentations through jhsingle-native-proxy, can be a string or list.
+        Default is ['jhsingle-native-proxy'].
         """
     ).tag(config=True)
 
